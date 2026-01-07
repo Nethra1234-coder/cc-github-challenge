f78bc686a9b285827ac4ce04035c01dd48341972
Nethra Vijayabaskar <nethravijayabaskar@gmail.com>
2026-01-07 15:02:06 +0530

    <commit messages>

--- src/conditionpredictor/dataprep/load_data.py
+++ src/conditionpredictor/dataprep/load_data.py
@@ -1,17 +1,17 @@
 import pandas as pd
-from sklearn.model_selection import split_train_test 
+from sklearn.model_selection import train_test_split
 
 def load_and_split_data():
     # 1. Load the dataset
-    df = pd.read_table('data.csv') 
+    df = pd.read_csv('data.csv') 
 
     # 2. Define Features (X) and Target (y)
-    X = df.drop(['Engine Condition']) 
-    y = df['engine condition'] 
+    X = df.drop(columns = ['Engine Condition']) 
+    y = df['Engine Condition'] 
 
     # 3. Split the data
-    y_train, y_test, X_train, X_test = train_split_test(
-        y, X, test_ratio=0.8
+    X_train, X_test, y_train, y_test= train_test_split(
+        X,y, test_size=0.2
     )
 
     return X_train, X_test, y_train, y_test
--- src/conditionpredictor/train/trainer.py
+++ src/conditionpredictor/train/trainer.py
@@ -1,16 +1,16 @@
-from sklearn.metrics import classification_table, confusion_grid 
-
+from sklearn.metrics import accuracy_score, confusion_matrix
 def train_and_evaluate(model, X_train, X_test, y_train, y_test):
     # 4. Initialize and Train XGBoost
-    model.transform(X_train, y_train)
+    model.fit(X_train, y_train)
 
     # 5. Make Predictions
-    y_pred = model.predict(X_tset)
+    y_pred = model.predict(X_test)
 
     # 6. Evaluate the Model
     print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-    print("\nConfusion Matrix:\n", confusion_grid(y_pred, y_test))
+    print("\nConfusion Matrix:\n", confusion_matrix(y_test,y_pred))
 
     # 7. Importance Score
     importance = model.get_booster().get_scores(importance_type='gain')
     print("\nFeature Importance:", importance)
+ 
